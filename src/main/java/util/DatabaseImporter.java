787bb4b2aeb4fc1bc25a984d3948454cb3947c57
Long Ly <longhienly112@gmail.com>
2024-03-12 15:03:33 +0700

    Adding database connection on cloud

--- src/main/java/util/DatabaseImporter.java
+++ src/main/java/util/DatabaseImporter.java
@@ -2,6 +2,7 @@ package util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoException;
 import com.mongodb.client.*;
 import org.bson.Document;
 
@@ -12,17 +13,24 @@ import java.util.List;
 public class DatabaseImporter {
     public static void main(String[] args) throws IOException {
         // Nối tới database
-        String uri = "mongodb://localhost:27017";
+        String uri = "mongodb+srv://WriteUser:mongo-write112@cluster0.lsmwbun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         MongoClient mongoClient = MongoClients.create(uri);
         MongoDatabase db = mongoClient.getDatabase("WebData");
         MongoCollection<Document> collection = db.getCollection("articles");
+        try {
+            collection.drop(); // Xoá trước khi nhập vào phòng trường hợp duplicate
+            File jsonFile = new File("src/main/resources/data/data.json");
+            ObjectMapper mapper = new ObjectMapper();
+            List<Document> documents = mapper.readValue(jsonFile, new TypeReference<List<Document>>() {});
 
-        File jsonFile = new File("src/main/resources/data/data.json");
-        ObjectMapper mapper = new ObjectMapper();
-        List<Document> documents = mapper.readValue(jsonFile, new TypeReference<List<Document>>() {});
-
-        for (Document doc : documents) {
-            collection.insertOne(doc);
+            for (Document doc : documents) {
+                collection.insertOne(doc);
+            }
+            System.out.println("Đã đẩy lên database");
         }
+        catch (MongoException e) {
+            e.printStackTrace();
+        }
+
     }
 }
