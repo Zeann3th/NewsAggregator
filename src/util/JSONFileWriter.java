377063f2f9ec80e1519c9eae63a556e24ff0872c
Long Ly <longhienly112@gmail.com>
2024-03-12 08:38:05 +0700

    Fixing json

--- src/util/JSONFileWriter.java
+++ src/util/JSONFileWriter.java
@@ -10,30 +10,32 @@ import java.util.List;
 
 public class JSONFileWriter {
     public void writeFile(List<Post> postList) {
-            JSONArray jArray = new JSONArray();
-            for (Post post : postList) {
-                JSONObject currentPost = new JSONObject();
-                currentPost.put("article_link", post.getArticleLink());
-                currentPost.put("website_source", post.getWebsiteSource());
-                currentPost.put("article_type", post.getArticleType());
-                currentPost.put("article_title", post.getArticleTitle());
+        JSONObject jsonFile = new JSONObject();
+        JSONArray jArray = new JSONArray();
+        for (Post post : postList) {
+            JSONObject currentPost = new JSONObject();
+            currentPost.put("article_link", post.getArticleLink());
+            currentPost.put("website_source", post.getWebsiteSource());
+            currentPost.put("article_type", post.getArticleType());
+            currentPost.put("article_title", post.getArticleTitle());
 
-                JSONObject authorJObject = new JSONObject();
-                authorJObject.put("name", post.getAuthor().getName());
-                authorJObject.put("last_post", post.getAuthor().getLastPost());
+            JSONObject authorJObject = new JSONObject();
+            authorJObject.put("name", post.getAuthor().getName());
+            authorJObject.put("last_post", post.getAuthor().getLastPost());
 
-                currentPost.put("author", authorJObject);
-                currentPost.put("creation_date", post.getCreationDate());
-                currentPost.put("article_summary", post.getArticleSummary());
-                currentPost.put("article_detailed_content", post.getArticleDetailedContent());
-                currentPost.put("associated_tags", post.getAssociatedTags());
-                currentPost.put("category", post.getCategory());
+            currentPost.put("author", authorJObject);
+            currentPost.put("creation_date", post.getCreationDate());
+            currentPost.put("article_summary", post.getArticleSummary());
+            currentPost.put("article_detailed_content", post.getArticleDetailedContent());
+            currentPost.put("associated_tags", post.getAssociatedTags());
+            currentPost.put("category", post.getCategory());
 
-                jArray.put(currentPost);
-            }
+            jArray.put(currentPost);
+        }
+        jsonFile.put("articles", jArray);
             try {
                 FileWriter writer = new FileWriter("json/data.json");
-                writer.write(jArray.toString());
+                writer.write(jsonFile.toString());
                 writer.close();
                 System.out.println("Dữ liêu đã được viết thành công!!!");
             }
--- src/webcrawling/CoinTelegraphCrawler.java
+++ src/webcrawling/CoinTelegraphCrawler.java
@@ -3,6 +3,9 @@ package webcrawling;
 import org.openqa.selenium.*;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import posts.Author;
 import posts.Post;
 
@@ -26,6 +29,16 @@ public class CoinTelegraphCrawler extends Crawler {
             // "Lăn chuột" để xuống cuối trang
             jse.executeScript("window.scrollBy(0,10000)", "");
             // Đợi 2,5 giây để trang load các bài báo tiếp theo
+//            try {
+//                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+//                wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".btn.posts-listing__more-btn.btn_pending"))));
+//            }
+//            catch (NoSuchElementException e) {
+//                e.printStackTrace();
+//            }
+//            catch (StaleElementReferenceException e) {
+//                e.printStackTrace();
+//            }
             try {
                 Thread.sleep(2500); // milliseconds
             } catch (InterruptedException e) {
@@ -72,7 +85,7 @@ public class CoinTelegraphCrawler extends Crawler {
                 continue;
             }
             postList.add(currentPost);
-            currentPost.display();
+//            currentPost.display();
             driver.navigate().back();
         }
         driver.quit();
