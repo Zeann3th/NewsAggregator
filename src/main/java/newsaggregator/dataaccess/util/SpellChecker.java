70b2b66457f579d5e96d11063a23816cf3275f08
Long Ly <longhienly112@gmail.com>
2024-03-13 20:36:37 +0700

    Testing spell cheking library

--- src/main/java/newsaggregator/dataaccess/util/SpellChecker.java
+++ src/main/java/newsaggregator/dataaccess/util/SpellChecker.java
@@ -1,5 +1,23 @@
 package newsaggregator.dataaccess.util;
 
-public class SpellChecker {
+import org.languagetool.JLanguageTool;
+import org.languagetool.Languages;
+import org.languagetool.rules.RuleMatch;
 
+import java.io.IOException;
+import java.util.List;
+
+public class SpellChecker {
+    //TODO: Find a way to change the incorrect string into the suggested string
+    public void check(String s) throws IOException {
+        JLanguageTool langTool = new JLanguageTool(Languages.getLanguageForShortCode("en-GB"));
+        List<RuleMatch> matches = langTool.check(s);
+        for (RuleMatch match : matches) {
+            System.out.println("Potential error at characters " +
+                    match.getFromPos() + "-" + match.getToPos() + ": " +
+                    match.getMessage());
+            System.out.println("Suggested correction(s): " +
+                    match.getSuggestedReplacements());
+        }
+    }
 }
