4ad05c4ce0b19581d9268d9fdc86b0706f071f06
Long Ly <longhienly112@gmail.com>
2024-03-13 19:57:58 +0700

    ...

--- src/main/java/newsaggregator/dataaccess/CheckConnection.java
+++ src/main/java/newsaggregator/dataaccess/CheckConnection.java
@@ -2,16 +2,15 @@ package newsaggregator.dataaccess;
 
 import com.mongodb.client.*;
 import org.bson.Document;
-import org.bson.json.JsonWriterSettings;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Connect {
+public class CheckConnection {
     public static void main(String[] args) {
         String connectionString = System.getProperty("mongodb.uri");
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            System.out.println("Kết nối thành công t database");
+            System.out.println("Kết nối thành công tới database");
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
         }
--- src/main/java/newsaggregator/dataaccess/ImportData.java
+++ src/main/java/newsaggregator/dataaccess/ImportData.java
@@ -3,21 +3,22 @@ package newsaggregator.dataaccess;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoException;
-import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class DatabaseOperations {
-    public void importJSON(String filePath) throws IOException{
+public class ImportData {
+    public void importJSON(String filePath) throws IOException {
         // Nối tới database
-        String uri = "<<connection string here>>"; // WriteUser
-        MongoClient mongoClient = MongoClients.create(uri);
-        MongoDatabase db = mongoClient.getDatabase("WebData");
-        MongoCollection<Document> collection = db.getCollection("articles");
-        try {
+        try (MongoClient mongoClient = MongoClients.create(System.getProperty("mongodb.uri"))){
+            MongoDatabase db = mongoClient.getDatabase("WebData");
+            MongoCollection<Document> collection = db.getCollection("articles");
             collection.drop(); // Xoá trước khi nhập vào phòng trường hợp duplicate
             File jsonFile = new File(filePath);
             ObjectMapper mapper = new ObjectMapper();
@@ -27,8 +28,6 @@ public class DatabaseOperations {
             }
             System.out.println("Đã đẩy lên database...");
         }
-        catch (MongoException e) {
-            e.printStackTrace();
-        }
     }
+    public void importCSV() {}
 }
